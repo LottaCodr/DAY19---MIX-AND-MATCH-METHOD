a04e15d3819acaa18df6e265e0f29fd48f88750e
King-Lota <lottanna47@gmail.com>
2023-01-12 21:10:38 +0100

    day17 - stateful favourite button class

---
+++ android/app/src/main/kotlin/com/example/learnstate/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.learnstate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const FavoriteWidget(),
+    );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({super.key});
+
+  @override
+  State<FavoriteWidget> createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavourite = true;
+  int _favouriteCount = 41;
+
+  void _toggleFavourite() {
+    // a state method to be called when the button is pressed
+    setState(() {
+      if (_isFavourite) {
+        _favouriteCount -= 1;
+        _isFavourite = false;
+      } else {
+        _favouriteCount += 1;
+        _isFavourite = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+              onPressed: _toggleFavourite,
+              icon: (_isFavourite
+                  ? const Icon(Icons.star)
+                  : const Icon(Icons.star_border)),
+              color: Colors.red[500]),
+        ),
+        SizedBox(
+          width: 18,
+          child: SizedBox(
+            child: Text('$_favouriteCount'),
+          ),
+        )
+      ],
+    );
+  }
+}
